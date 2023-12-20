c412b06db93f93fb35f74995e2d533efecb3a6e7
Usama Munir <usama_1s@hotmail.com>
2023-12-21 01:20:09 +0500

    SEO improved

---
+++ assets/js/main.js
@@ -0,0 +1,140 @@
+// navbar animation
+$(".navbar-collapse")
+.on("show.bs.collapse", function () {
+  $(this).addClass("show");
+})
+.on("hide.bs.collapse", function () {
+  $(this).removeClass("show");
+});
+
+// document.querySelector(".sidebar-toggler").addEventListener("click", function () {
+
+//   if (document.querySelector(".sidebar-toggler").classList.contains("collapsed")) {
+//     console.log("close");
+//   }else{
+// console.log("open");
+  // }
+  // document.querySelector(".navbar-collapse").classList.add("show");
+// });
+
+// document.querySelector(".navbar-collapse").addEventListener("hide.bs.collapse", function () {
+//   document.querySelector(".navbar-collapse").classList.remove("show");
+// });
+
+// custom sliders
+document
+.querySelectorAll(".slider-range")
+.forEach(function (singleSlider) {
+  const slider = singleSlider.querySelector(".slider");
+  const sliderValue = singleSlider.querySelector(".value");
+  slider.addEventListener("input", (event) => {
+    const tempSliderValue = event.target.value;
+    sliderValue.textContent = tempSliderValue;
+    const progress = ((tempSliderValue - slider.min) / (slider.max - slider.min)) * 100;
+
+    slider.style.background = `linear-gradient(to right, #97d3fd ${progress}%, #f4f4f4 ${progress}%)`;
+  });
+});
+
+// multistep form
+document.querySelectorAll(".multistep-form").forEach(function (thisForm) {
+thisForm.querySelectorAll(".single-step").forEach(function (thisStep) {
+  const nextBtn = thisStep.querySelector(".next-multistep-form");
+  if (nextBtn) {
+    nextBtn.addEventListener("click", (event) => {
+      currentStep = nextBtn.closest(".single-step");
+
+      nextStep = currentStep.nextElementSibling;
+      if (nextStep) {
+        currentStep.classList.remove("active");
+        nextStep.classList.add("active");
+      }
+    });
+  }
+});
+});
+
+// disable all body when menu opens
+
+// listen to class change on button to remove delay in animation
+// const sidebarToggler = document.querySelector(".sidebar-toggler");
+
+const options = {
+attributes: true,
+};
+// function callback(mutationList1, observer1) {
+// mutationList1.forEach(function (mutation) {
+//   console.log("fsda");
+//   if (
+//     mutation.type === "attributes" &&
+//     mutation.attributeName === "class"
+//   ) {
+//     if (
+//       document
+//         .getElementsByClassName("sidebar-toggler")[0]
+//         .classList.contains("collapsed")
+//     ) {
+//       console.log("yes");
+//     } else {
+//       console.log("no");
+//     }
+//   }
+// });
+// }
+// const observer1 = new MutationObserver(callback);
+// observer1.observe(sidebarToggler, options);
+
+
+
+// listen to class change to add menu-open
+const sidebar = document.querySelector(".sidebar");
+
+function callback(mutationList, observer) {
+mutationList.forEach(function (mutation) {
+ 
+  if (
+    mutation.type === "attributes" &&
+    mutation.attributeName === "class"
+  ) {
+    if (
+      document
+        .getElementsByClassName("sidebar")[0]
+        .classList.contains("show")
+    ) {
+      // console.log("open");
+      document.body.classList.add("menu-open");
+    } else {
+      // console.log("close");
+      document.body.classList.remove("menu-open");
+    }
+  }
+});
+}
+const observer = new MutationObserver(callback);
+observer.observe(sidebar, options);
+
+// close menu on outside click
+document.querySelector(".menu-overlay").addEventListener("click", () => {
+if (
+  document
+    .getElementsByClassName("sidebar")[0]
+    .classList.contains("show")
+) {
+  document.querySelector(".sidebar-toggler").click();
+}
+});
+
+// close on menu on sidebar change
+const navLinks = document.querySelectorAll(".sidebar-links a");
+
+document.querySelectorAll(".sidebar-links a").forEach(function (navLink) {
+navLink.addEventListener("click", () => {
+  if (
+    document
+      .getElementsByClassName("sidebar")[0]
+      .classList.contains("show")
+  ) {
+    document.querySelector(".sidebar-toggler").click();
+  }
+});
+});
